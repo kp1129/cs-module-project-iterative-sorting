8ad9a3023e20a6fefc8611708eee7aa7ed6c3652
kp1129 <katya.p.1129@gmail.com>
2020-07-20 13:37:55 -0700

    complete searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,22 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    first = 0
+    last = len(arr) - 1
+
+    while first <= last:
+        middle = (first + last) // 2
+
+        if arr[middle] == target:
+            return middle
+
+        else:
+            if target < arr[middle]:
+                last = middle - 1
+
+            else:
+                first = middle + 1
+                        
 
 
     return -1  # not found
